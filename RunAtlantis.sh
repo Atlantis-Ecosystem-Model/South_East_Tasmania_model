2bb2bc2eace2e3c2ed8cb46d73f847807debf71a
jporobicg <jporobicg@gmail.com>
2018-11-07 08:16:22 +1100

    update code

---
+++ RunAtlantis.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+atlantisMerged -i Initial_condition.nc 0 -o outputsSETas.nc -r Run_settings.prm -f Forcings.prm -p Physics.prm -b Biology.prm -h Harvest.prm -s Functional_groups.csv -q Fisheries.csv -d SETasOut_1
