f5d47c1611685ce60627719609b922a18b82baf2
eafulton <beth.fulton@csiro.au>
2022-05-02 19:16:21 +1000

    Updated migration handling
    This has expanded the parameter files for new parameters and especially the migration file

--- RunAtlantis.sh
+++ RunAtlantis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-atlantisMerged -i Initial_condition.nc 0 -o outputs.nc -r Run_settings.prm -f Forcings.prm -p Physics.prm -b Biology.prm -h Harvest.prm -s Functional_groups.csv -q Fisheries.csv -d output
+atlantisMerged -i Initial_condition.nc 0 -o outputs.nc -r Run_settings.prm -f Forcings.prm -p Physics.prm -b Biology.prm -m Migrations.csv -h Harvest.prm -s Functional_groups.csv -q Fisheries.csv -d output
